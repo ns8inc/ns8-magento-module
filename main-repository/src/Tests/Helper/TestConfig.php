d907378109fb584a84ef45f801e239902ec54bff
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-08-22 12:17:37 -0400

    First unit test.

---
+++ main-repository/src/Tests/Helper/TestConfig.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace NS8\CSP2\Tests\Helper;
+
+class TestConfig extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Is called once before running all test in class
+     */
+    public static function setUpBeforeClass()
+    {
+    }
+
+    /**
+     * Is called once after running all test in class
+     */
+    public static function tearDownAfterClass()
+    {
+    }
+
+    /**
+     * Is called before running a test
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Is called after running a test
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * The test itself, every test function must start with 'test'
+     */
+    public function testTest()
+    {
+        $this->assertTrue(false);
+    }
+}
