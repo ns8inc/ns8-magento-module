df5fbc740502df4d4e49dcef49bd4c027583fc7e
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-08-23 10:17:34 -0400

    Prepping for eventual ESLint support.

---
+++ main-repository/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {    },
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+};
