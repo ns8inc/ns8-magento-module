9497003ced285469d9575436010f008ddbac8ce1
David Watson <davidlwatsonjr@users.noreply.github.com>
2019-09-11 12:40:17 -0400

    Add a Transaction adapter

---
+++ package/switches/src/adapters/Transaction.ts
@@ -0,0 +1,203 @@
+// To parse this data:
+//
+//   import { Convert } from "./file";
+//
+//   const transaction = Convert.toTransaction(json);
+//
+// These functions will throw an error if the JSON doesn't
+// match the expected interface, even if the JSON is valid.
+
+export interface Transaction {
+  transactionID:         number;
+  parentID:              number | null;
+  orderID:               number;
+  paymentID:             number;
+  txnID:                 string;
+  parentTxnID:           string;
+  txnType:               TxnType;
+  isClosed:              number;
+  additionalInformation: string[] | null;
+  createdAt:             string;
+  childTransactions:     Transaction[];
+  extensionAttributes:   ExtensionAttributes | null;
+}
+
+export interface ExtensionAttributes {
+}
+
+export enum TxnType {
+  Payment = "payment",
+  Order = "order",
+  Authorization = "authorization",
+  Capture = "capture",
+  Void = "void",
+  Refund = "refund",
+}
+
+// Converts JSON strings to/from your types
+// and asserts the results of JSON.parse at runtime
+export class Convert {
+  public static toTransaction(json: string): Transaction[] {
+      return cast(JSON.parse(json), a(r("Transaction")));
+  }
+
+  public static transactionToJson(value: Transaction[]): string {
+      return JSON.stringify(uncast(value, a(r("Transaction"))), null, 2);
+  }
+}
+
+function invalidValue(typ: any, val: any): never {
+  throw Error(`Invalid value ${JSON.stringify(val)} for type ${JSON.stringify(typ)}`);
+}
+
+function jsonToJSProps(typ: any): any {
+  if (typ.jsonToJS === undefined) {
+      var map: any = {};
+      typ.props.forEach((p: any) => map[p.json] = { key: p.js, typ: p.typ });
+      typ.jsonToJS = map;
+  }
+  return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ: any): any {
+  if (typ.jsToJSON === undefined) {
+      var map: any = {};
+      typ.props.forEach((p: any) => map[p.js] = { key: p.json, typ: p.typ });
+      typ.jsToJSON = map;
+  }
+  return typ.jsToJSON;
+}
+
+function transform(val: any, typ: any, getProps: any): any {
+  function transformPrimitive(typ: string, val: any): any {
+      if (typeof typ === typeof val) return val;
+      return invalidValue(typ, val);
+  }
+
+  function transformUnion(typs: any[], val: any): any {
+      // val must validate against one typ in typs
+      var l = typs.length;
+      for (var i = 0; i < l; i++) {
+          var typ = typs[i];
+          try {
+              return transform(val, typ, getProps);
+          } catch (_) {}
+      }
+      return invalidValue(typs, val);
+  }
+
+  function transformEnum(cases: string[], val: any): any {
+      if (cases.indexOf(val) !== -1) return val;
+      return invalidValue(cases, val);
+  }
+
+  function transformArray(typ: any, val: any): any {
+      // val must be an array with no invalid elements
+      if (!Array.isArray(val)) return invalidValue("array", val);
+      return val.map(el => transform(el, typ, getProps));
+  }
+
+  function transformDate(typ: any, val: any): any {
+      if (val === null) {
+          return null;
+      }
+      const d = new Date(val);
+      if (isNaN(d.valueOf())) {
+          return invalidValue("Date", val);
+      }
+      return d;
+  }
+
+  function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
+      if (val === null || typeof val !== "object" || Array.isArray(val)) {
+          return invalidValue("object", val);
+      }
+      var result: any = {};
+      Object.getOwnPropertyNames(props).forEach(key => {
+          const prop = props[key];
+          const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+          result[prop.key] = transform(v, prop.typ, getProps);
+      });
+      Object.getOwnPropertyNames(val).forEach(key => {
+          if (!Object.prototype.hasOwnProperty.call(props, key)) {
+              result[key] = transform(val[key], additional, getProps);
+          }
+      });
+      return result;
+  }
+
+  if (typ === "any") return val;
+  if (typ === null) {
+      if (val === null) return val;
+      return invalidValue(typ, val);
+  }
+  if (typ === false) return invalidValue(typ, val);
+  while (typeof typ === "object" && typ.ref !== undefined) {
+      typ = typeMap[typ.ref];
+  }
+  if (Array.isArray(typ)) return transformEnum(typ, val);
+  if (typeof typ === "object") {
+      return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+          : typ.hasOwnProperty("arrayItems")    ? transformArray(typ.arrayItems, val)
+          : typ.hasOwnProperty("props")         ? transformObject(getProps(typ), typ.additional, val)
+          : invalidValue(typ, val);
+  }
+  // Numbers can be parsed by Date but shouldn't be.
+  if (typ === Date && typeof val !== "number") return transformDate(typ, val);
+  return transformPrimitive(typ, val);
+}
+
+function cast<T>(val: any, typ: any): T {
+  return transform(val, typ, jsonToJSProps);
+}
+
+function uncast<T>(val: T, typ: any): any {
+  return transform(val, typ, jsToJSONProps);
+}
+
+function a(typ: any) {
+  return { arrayItems: typ };
+}
+
+function u(...typs: any[]) {
+  return { unionMembers: typs };
+}
+
+function o(props: any[], additional: any) {
+  return { props, additional };
+}
+
+function m(additional: any) {
+  return { props: [], additional };
+}
+
+function r(name: string) {
+  return { ref: name };
+}
+
+const typeMap: any = {
+  "Transaction": o([
+      { json: "transaction_id", js: "transactionID", typ: 0 },
+      { json: "parent_id", js: "parentID", typ: u(0, null) },
+      { json: "order_id", js: "orderID", typ: 0 },
+      { json: "payment_id", js: "paymentID", typ: 0 },
+      { json: "txn_id", js: "txnID", typ: "" },
+      { json: "parent_txn_id", js: "parentTxnID", typ: "" },
+      { json: "txn_type", js: "txnType", typ: u(undefined, r("TxnType")) },
+      { json: "is_closed", js: "isClosed", typ: 0 },
+      { json: "additional_information", js: "additionalInformation", typ: u(a(""), null) },
+      { json: "created_at", js: "createdAt", typ: "" },
+      { json: "child_transactions", js: "childTransactions", typ: a(r("Transaction")) },
+      { json: "extension_attributes", js: "extensionAttributes", typ: u(r("ExtensionAttributes"), null) },
+  ], false),
+  "ExtensionAttributes": o([
+  ], false),
+  "TxnType": [
+    "payment",
+    "order",
+    "authorization",
+    "capture",
+    "void",
+    "refund",
+],
+};
