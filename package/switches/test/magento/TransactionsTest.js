9c8441d2dfeacad20d1cc97121de788b512d4eb3
David Watson <davidlwatsonjr@users.noreply.github.com>
2019-09-17 11:57:35 -0400

    Accommodate lack of default export in MagentoClient

--- package/switches/test/magento/TransactionsTest.js
+++ package/switches/test/magento/TransactionsTest.js
@@ -1,15 +1,5 @@
-const Magento2Client = require('@ns8/magento2-rest-client').Magento2Client;
+const MagentoClient = require('../../dist/core/MagentoClient').MagentoClient;
 
-const result = require('dotenv').config();
-
-const MagentoClient = Magento2Client({
-  url: process.env.URL,
-  consumerKey: process.env.CONSUMER_KEY,
-  consumerSecret: process.env.CONSUMER_SECRET,
-  accessToken: process.env.ACCESS_TOKEN,
-  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
-});
-
-MagentoClient.transactions.list().then(function(transactions) {
+MagentoClient.transactions.list({}).then(function(transactions) {
   console.log(transactions);
 });
