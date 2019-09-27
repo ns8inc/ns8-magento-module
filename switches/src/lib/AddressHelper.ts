c5f370cc1b35c05020dde0807778a9ba7d3c5ddf
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-09-27 14:04:21 -0400

    Added utility functions for common operations, including `Safely` which has methods to safely get properties from objects.

--- switches/src/lib/AddressHelper.ts
+++ switches/src/lib/AddressHelper.ts
@@ -1,19 +1,32 @@
 import { SwitchContext } from "ns8-switchboard-interfaces";
 import { MagentoClient, SessionHelper } from ".";
 import { Address } from "ns8-protect-models";
+import { Order } from '@ns8/magento2-rest-client';
+import { toProtectAddressType, Safefly } from "./util";
 
 export class AddressHelper {
   private SwitchContext: SwitchContext;
   private MagentoClient: MagentoClient;
-
+  private MagentoOrder: Order;
   constructor(switchContext: SwitchContext, magentoClient: MagentoClient) {
     this.SwitchContext = switchContext;
     this.MagentoClient = magentoClient;
+    this.MagentoOrder = switchContext.data.order as Order;
   }
 
   public toAddresses = (): Address[] => {
+    const ret: Address[] = [];
+    this.MagentoOrder.extension_attributes.shipping_assignments.forEach((assignment) => {
+      var address = new Address({
+        type: toProtectAddressType(assignment.shipping.address.address_type)
 
-    return [];
+      });
+      address.address1 = Safefly.get(assignment, 'shipping.address.street.0');
+      address.address2 = Safefly.get(assignment, 'shipping.address.street.1');
+      ret.push(address);
+    });
+
+    return ret;
   }
 
 }
