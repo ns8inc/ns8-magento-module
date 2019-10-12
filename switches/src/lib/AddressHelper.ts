c2df0fe0901b88e642dcff28b12c221fd3ce12de
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-12 00:07:14 -0400

    Remove casts to `unknown` as intermediate proxy for type casting. Use `any` for Magento Address types (for now). https://github.com/ns8inc/ns8-magento-platform/pull/16#discussion_r334134945

--- switches/src/lib/AddressHelper.ts
+++ switches/src/lib/AddressHelper.ts
@@ -4,7 +4,7 @@ import { error, MagentoClient } from '.';
 import { ModelTools } from '@ns8/ns8-protect-sdk';
 import { Order as MagentoOrder } from '@ns8/magento2-rest-client';
 import { SwitchContext } from 'ns8-switchboard-interfaces';
-import { ShippingAssignment as MagentoShippingAssignment, } from '@ns8/magento2-rest-client';
+import { Address as MagentoAddress } from '@ns8/magento2-rest-client';
 import { getName } from 'country-list';
 
 /**
@@ -27,17 +27,20 @@ export class AddressHelper {
   public toOrderAddresses = (): Address[] => {
     const ret: Address[] = [];
     try {
-      const addresses: MagentoShippingAssignment[] = [];
+      //For now, use `any` instead of the MagentoAddress or MagentoShippingAssignment types as neither type actually matches the data returned from the API
+      //This addresses the problem of Address.region sometimes being a Region object vs a string;
+      //  the sometimes present`address_type`; and the inconsistencies between the billing address data vs the shipping address data.
+      const addresses: any[] = [];
 
       //Billing Address is stored separate from the shipping address
-      const billingAddress = this.MagentoOrder.billing_address as unknown as MagentoShippingAssignment;
+      const billingAddress = this.MagentoOrder.billing_address as any;
       addresses.push(billingAddress);
 
       //In theory, there should never be more than one shipping address, as Magento's default behavior is to automatically split orders shipped to different addresses into separate, distinct Orders.
       //However, since the data is stored as an array, it is best to assume that at some point there could be more than one shipping address.
       const shipping_assignments = get(this.MagentoOrder, 'extension_attributes.shipping_assignments') || [];
       shipping_assignments.forEach((assignment) => {
-        addresses.push(get(assignment, 'shipping.address') as MagentoShippingAssignment);
+        addresses.push(get(assignment, 'shipping.address'));
       });
       addresses.forEach((address) => {
         const addr = new Address({
