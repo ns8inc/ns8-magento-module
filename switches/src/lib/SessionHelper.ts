f95579dec881c6a7579dce465ea436835b19b0fa
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-13 11:41:56 -0400

    Better default IP address if we can get it. Orthogonal to [ch9740].

--- switches/src/lib/SessionHelper.ts
+++ switches/src/lib/SessionHelper.ts
@@ -3,11 +3,19 @@ import { HelperBase } from './HelperBase';
 
 export class SessionHelper extends HelperBase {
 
+  private getIpAddress = ():string => {
+    //NOTE: for mock purposes, this must be any real value that is not localhost, 127.0.0.1 or otherwise a reserved "localhost" IP address
+    let ret = '1.1.1.1'; //DNS Server IP
+    if (this.SwitchContext.data && this.SwitchContext.data.order && this.SwitchContext.data.order.remote_ip) {
+      ret = this.SwitchContext.data.order.remote_ip;
+    }
+    return ret;
+  }
+
   //TODO: ship this data from Magento
   public toSession = (): Session => {
     return new Session({
-      //NOTE: for mock purposes, this must be any real value that is not localhost, 127.0.0.1 or otherwise a reserved "localhost" IP address
-      ip: '1.1.1.1',
+      ip: this.getIpAddress(),
     });
   }
 }
