8fa7641f7fca4ee693d89d90d71d476d95329774
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-13 10:55:42 -0400

    Added `ts-node` for the execution of build scripts. Removed build scripts from tsconfig. Added .env.template file for reference. Added SCP script to deploy to Magento from yarn.

--- build/build.ts
+++ build/build.ts
@@ -2,7 +2,4 @@ import { BuildTools } from '@ns8/ns8-protect-sdk';
 
 //Sort all the JSON files to improve readability and reduce conflicts
 BuildTools.sortJsonFiles();
-
-//Eventually: Clone the root TS Config down to the sub-projects
-//copyFileSync('../tsconfig.json', '../switchboard/tsconfig.json');
-//copyFileSync('../tsconfig.json', '../switches/tsconfig.json');
+console.info('Alpha-sorted all JSON files');
