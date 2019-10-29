5615968a2c135c1920ecb9f86bbf9ec64fe25e6d
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-10-29 17:04:47 -0400

    Missed a constant.

--- module/Helper/Config.php
+++ module/Helper/Config.php
@@ -248,7 +248,7 @@ class Config extends AbstractHelper
 
     public function getExtensionVersion()
     {
-        return $this->moduleList->getOne('NS8_Protect')['setup_version'];
+        return $this->moduleList->getOne(Config::NS8_MODULE_NAME)['setup_version'];
     }
 
     //  needed for install/upgrade routines - do not call from anywhere else
