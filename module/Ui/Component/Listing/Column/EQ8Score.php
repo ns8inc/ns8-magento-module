ee76b24c8371e99ff340b5989aec5fa0eb1c3dd2
David Watson <david.watson@ns8.com>
2019-11-01 14:50:25 +0000

    Populate EQ8 Score Column with random scores

---
+++ module/Ui/Component/Listing/Column/EQ8Score.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace NS8\Protect\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * EQ8Score Column Class
+ *
+ * This class handles populating the data necessary for the
+ * EQ8 Score Column in the Sales Order Grid
+ */
+class EQ8Score extends Column
+{
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context The Magento Context
+     * @param UiComponentFactory $uiComponentFactory The UI Component Factory
+     * @param array $components The components
+     * @param array $data The data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Loop through the Orders and check for an EQ8 Score
+     * If none is present
+     *  - Fetch it from Protect
+     *  - Persist it
+     *
+     * @param array $dataSource The Orders we'll be looping through
+     *
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource): array
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item['eq8_score'] = strval(rand(1, 1000));
+            }
+        }
+
+        return $dataSource;
+    }
+}
