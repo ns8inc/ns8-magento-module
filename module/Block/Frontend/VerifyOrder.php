60dd8359d1bf037461a13d324bdc19cc05f1efaa
Dave Hensley <dave.hensley@ns8.com>
2019-11-07 09:26:38 -0500

    [ch10936] Add customer SMS verification workflow.

--- module/Block/Frontend/VerifyOrder.php
+++ module/Block/Frontend/VerifyOrder.php
@@ -56,19 +56,90 @@ class VerifyOrder extends Template
     }
 
     /**
-     * Makes a call to NS8 Protect API to either approve or cancel the order.
+     * Makes a call to the NS8 Template Service to grab the requested template.
      *
-     * @return string The new order status if it was successfully changed, null otherwise.
+     * There's a lot of hardcoded Shopify-specific stuff in the templates that the service spits out, so we need to
+     * hack them up a bit to get them to work in Magento. We really need to fix it up in the Template Service.
+     *
+     * @throws Exception If a valid view was not specified in the URL.
+     *
+     * @return string The template (HTML)
      */
-    public function updateOrderStatus(): ?string {
-        $orderId = $this->request->getParam('orderId');
-        $newStatus = ($this->request->getParam('view') === 'orders-validate') ? 'APPROVED' : 'CANCELLED';
-        $response = $this->httpClient->put(sprintf('orders/%s', $orderId), ['status' => $newStatus]);
+    public function getNS8Template(): string
+    {
+        $params = $this->request->getParams();
 
-        if (isset($response->status) && is_string($response->status)) {
-            return $response->status;
+        $validViews = [
+            'orders-reject',
+            'orders-reject-confirm',
+            'orders-validate',
+            'orders-validate-code',
+        ];
+
+        if (!in_array($params['view'] ?? null, $validViews)) {
+            throw new Exception('No valid view was specified in the URL.');
         }
 
-        return null;
+        if ($this->request->isPost()) {
+            $postFields = array_merge($params, (array)$this->request->getPost(), [
+                'returnUri' => implode('/', [
+                    $this->getBaseUrl(),
+                    'ns8csp2',
+                    'order',
+                    'verify',
+                    'orderId', '{orderId}',
+                    'verificationId', '{verificationId}',
+                    'view', '{view}',
+                ]),
+            ]);
+
+            $response = $this->httpClient->post('merchant/template', $postFields);
+        } else {
+            $response = $this->httpClient->get(sprintf('merchant/template?%s', http_build_query($params)));
+        }
+
+        return isset($response->location)
+            ? $this->redirect($response->location)
+            : $this->unShopifyNS8Template($response->html);
+    }
+
+    /**
+     * Generate some JS that will make the user's browser redirect to a new URL.
+     *
+     * @param string $url The URL that the user should get redirected to
+     *
+     * @return string The JS code (including <script> tags).
+     */
+    private function redirect(string $url): string
+    {
+        return sprintf('<script>window.location.replace("%s");</script>', htmlspecialchars($url));
+    }
+
+    /**
+     * This is a crappy, hopefully temporary method to remove all Shopify-specific stuff from the NS8 template
+     * and replace it with something more palatable to Magento.
+     *
+     * @param string $template The NS8 template
+     *
+     * @return string The converted template
+     */
+    private function unShopifyNS8Template(string $template): string
+    {
+        // If we find an HTML body tag, we're only interested in the stuff inside it (not the html/head/etc. tags).
+        if (preg_match('/<body>(.*)(<\/body>)/s', $template, $matches)) {
+            $template = $matches[1];
+        }
+
+        $template = preg_replace_callback(
+            '#"(/apps/ns8-protect.*?)"#',
+            function (array $matches): string {
+                $query = parse_url($matches[1], PHP_URL_QUERY);
+
+                return sprintf('%s/ns8csp2/order/verify/%s', $this->getBaseUrl(), str_replace(['=', '&'], '/', $query));
+            },
+            $template
+        );
+
+        return $template;
     }
 }
