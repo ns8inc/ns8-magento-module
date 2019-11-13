f7a6c8578c9e8ad8b7e6397998d730b7e6fac844
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-11-13 10:29:22 -0500

    Updated to latest version of the rest client to get updated merchant update data model from [ch10362]. Implement new type and add guards for newly exposed possible undefined values.

--- switches/src/lib/MerchantHelper.ts
+++ switches/src/lib/MerchantHelper.ts
@@ -1,4 +1,5 @@
 import { MerchantUpdate, Contact } from 'ns8-protect-models';
+import { MerchantUpdate as MagentoMerchantUpdate } from '@ns8/magento2-rest-client';
 import { SwitchContext } from 'ns8-switchboard-interfaces';
 import { Logger } from '@ns8/ns8-protect-sdk';
 
@@ -17,35 +18,45 @@ export class MerchantHelper {
     const ret = new MerchantUpdate(this.SwitchContext.merchant);
     try {
       ret.contact = new Contact(ret.contact);
-      const configData = (this.SwitchContext.data.eventData && this.SwitchContext.data.eventData.configData) ?
+      const configData: MagentoMerchantUpdate | undefined = (this.SwitchContext.data.eventData && this.SwitchContext.data.eventData.configData) ?
         this.SwitchContext.data.eventData.configData :
         this.SwitchContext.data.configData;
 
       if (!configData || !configData.groups) return ret;
 
-      if (configData.groups.store_information) {
+      if (configData.groups.store_information && configData.groups.store_information.fields) {
         const storeInformation = configData.groups.store_information.fields;
-        ret.contact.phone = storeInformation.phone.value;
-        ret.contact.name = storeInformation.name.value;
-        ret.name = storeInformation.name.value;
+        ret.contact.phone = (storeInformation.phone) ? storeInformation.phone.value : undefined;
+        ret.contact.name = (storeInformation.name) ? storeInformation.name.value : undefined;
+        ret.name = (storeInformation.name) ? storeInformation.name.value : undefined;
       }
 
-      if (configData.groups.ident_general) {
+      if (configData.groups.ident_general && configData.groups.ident_general.fields) {
         const generalIdInfo = configData.groups.ident_general.fields;
-        const names: string[] = generalIdInfo.name.value.split(' ');
+        const names: string[] = (generalIdInfo.name && generalIdInfo.name.value) ? generalIdInfo.name.value.split(' ') : [];
         ret.contact.firstName = names.shift();
         ret.contact.lastName = names.join(' ');
-        ret.contact.email = generalIdInfo.email.value;
+        ret.contact.email = (generalIdInfo.email) ? generalIdInfo.email.value : ;
       }
 
-      if (configData.groups.unsecure && configData.groups.secure) {
-        const unsecureBaseUrl: string = configData.groups.unsecure.fields.base_url.value;
-        const secureBaseUrl: string = configData.groups.secure.fields.base_url.value;
-        const useSecureBaseUrl: boolean = (configData.groups.secure.fields.use_in_frontend.value === '1');
-        const storefrontUrl: string = useSecureBaseUrl ? secureBaseUrl : unsecureBaseUrl;
-        ret.storefrontUrl = storefrontUrl.replace(/\/$/, '');
+      if ((configData.groups.unsecure && configData.groups.unsecure.fields) || (configData.groups.secure && configData.groups.secure.fields) {
+        const unsecureBaseUrl: string | undefined = (configData.groups.unsecure &&
+          configData.groups.unsecure.fields &&
+          configData.groups.unsecure.fields.base_url) ?
+          configData.groups.unsecure.fields.base_url.value : undefined;
+        const secureBaseUrl: string | undefined = (configData.groups.secure &&
+          configData.groups.secure.fields &&
+          configData.groups.secure.fields.base_url) ?
+          configData.groups.secure.fields.base_url.value : undefined;
+        const useSecureBaseUrl: boolean | undefined = (configData.groups.secure &&
+          configData.groups.secure.fields &&
+          configData.groups.secure.fields.use_in_frontend &&
+          configData.groups.secure.fields.use_in_frontend.value === '1');
+        const storefrontUrl: string | undefined = useSecureBaseUrl ? secureBaseUrl : unsecureBaseUrl;
+        if (storefrontUrl) {
+          ret.storefrontUrl = storefrontUrl.replace(/\/$/, '');
+        }
       }
-
     } catch (e) {
       Logger.error(`Failed to create Merchant update event`, e);
     }
