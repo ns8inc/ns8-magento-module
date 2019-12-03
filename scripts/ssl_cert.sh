eabb189d10395de5170a73242d6b2b47e8f88d94
David Watson <david.watson@ns8.com>
2019-12-03 22:01:27 +0000

    Add case to ssl_cert to cancel if certification wasn't successful

--- scripts/ssl_cert.sh
+++ scripts/ssl_cert.sh
@@ -2,6 +2,12 @@
 
 # Begin interactive certbot certification
 sudo /usr/local/bin/certbot-auto --apache --redirect
+# Ensure certification was successful before moving on
+RETURN_CODE=$?
+if [ $RETURN_CODE != 0 ]
+then
+  exit $RETURN_CODE
+fi
 
 # Configure Magento to use secure Urls
 sudo -u apache php /var/www/html/bin/magento setup:store-config:set --use-secure-admin=1
