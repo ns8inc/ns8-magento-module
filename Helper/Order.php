bc249c2fcaaca42f492952cc958fcae14ab10296
Concourse CI <noreply@ns8.com>
2020-01-13 21:57:13 +0000

    Automatic update.

--- Helper/Order.php
+++ Helper/Order.php
@@ -17,6 +17,9 @@ use UnexpectedValueException;
  */
 class Order extends AbstractHelper
 {
+
+    public const EQ8_SCORE_COL = 'eq8_score';
+
     /**
      * @var HttpClient
      */
@@ -112,7 +115,7 @@ class Order extends AbstractHelper
         if (!isset($order)) {
             throw new UnexpectedValueException('Order Id: '.$orderId.' could not be found');
         }
-        $eq8Score = $order->getData('eq8_score');
+        $eq8Score = $order->getData(self::EQ8_SCORE_COL);
         if (isset($eq8Score)) {
             return $eq8Score;
         }
@@ -149,7 +152,7 @@ class Order extends AbstractHelper
     public function setEQ8Score(int $eq8Score, OrderInterface $order) : int
     {
         $order
-            ->setData('eq8_score', $eq8Score)
+            ->setData(self::EQ8_SCORE_COL, $eq8Score)
             ->save();
 
         return $eq8Score;
@@ -157,20 +160,30 @@ class Order extends AbstractHelper
 
     /**
      * Get an EQ8 Score from an order id. If it does not exist locally, fetch it from the API and store it.
-     * @param string|null $orderId
+     * @param OrderInterface $order
      * @return string An EQ8 Score link for this order
      */
-    public function getEQ8ScoreLinkHtml(string $orderId = null): string
+    public function getEQ8ScoreLinkHtml(OrderInterface $order): string
     {
-        if (!isset($orderId)) {
-            return 'NA';
-        }
-        $eq8Score = $this->getEQ8Score($orderId);
-        if (!isset($eq8Score)) {
+        $orderId = isset($order) ? $order->getId() : null;
+        $eq8Score = isset($orderId)
+            ? $this->getEQ8Score($orderId)
+            : null;
+        return $this->formatEQ8ScoreLinkHtml($orderId, $eq8Score);
+    }
+
+    /**
+     * Format an EQ8 Score and orderId as a link, or return "NA" if either value is `null`
+     * @param string orderId
+     * @param int $eq8Score
+     * @param string An HTML anchor tag with the score and an href to the order details
+     */
+    public function formatEQ8ScoreLinkHtml(?string $orderId, ?int $eq8Score): string
+    {
+        if (!isset($orderId) || !isset($eq8Score)) {
             return 'NA';
         }
         $link = $this->url->getNS8IframeUrl(['page' => 'order_details', 'order_id' => $orderId]);
-
         return '<a href="'.$link.'">'.$eq8Score.'</a>';
     }
 }
