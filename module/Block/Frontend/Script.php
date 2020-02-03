aef70bc5216d0905c1ce68b023552394ab8b5492
Chris <chris@hnas-MacBook-Pro.local>
2020-02-03 09:19:48 -0800

    Updating Script fetching logic

--- module/Block/Frontend/Script.php
+++ module/Block/Frontend/Script.php
@@ -12,7 +12,7 @@ namespace NS8\Protect\Block\Frontend;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
-use NS8\Protect\Helper\Url;
+use NS8\Protect\Helper\Config;
 use NS8\ProtectSDK\Http\Client as HttpClient;
 
 /**
@@ -23,22 +23,34 @@ use NS8\ProtectSDK\Http\Client as HttpClient;
 class Script extends Template
 {
     /**
-     * The URL Helper class
-     *
-     * @var Url
+     * @var Config
      */
-    public $url;
+    protected $config;
 
-    /**
-     * The constructor.
-     *
-     * @param Context $context The Magento context
-     * @param Url $url The URL Helper attribute
-     * @param array $data The data to pass to the Template constructor (optional)
-     */
-    public function __construct(Context $context, Url $url, array $data = [])
+     /**
+      * The constructor.
+      *
+      * @param Context $context The Magento context
+      * @param Config $config The Config Helper attribute
+      * @param array $data The data to pass to the Template constructor (optional)
+      */
+    public function __construct(Context $context, Config $config, array $data = [])
     {
         parent::__construct($context, $data);
-        $this->url = $url;
+        $this->config = $config;
+    }
+
+    /**
+     * Get the TrueStats tracking script (wrapped in HTML <script> tags).
+     *
+     * @return string The tracking script
+     */
+    public function getScriptHtml(): string
+    {
+        $this->config->initSdkConfiguration();
+        $script = (new HttpClient())->sendNonObjectRequest('/init/script');
+
+        // Call json_decode to remove quotes if present
+        return is_string($script) ? sprintf('<script>%s</script>', json_decode($script)) : '';
     }
 }
