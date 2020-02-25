c363870a1609592a5a3c9d74a522c9e62e2774cb
Dave Hensley <dave.hensley@ns8.com>
2020-02-25 12:18:13 -0500

    Allow overriding the default SSH port when deploying the Magento module. CH-16146

--- build/moduleDeploy.ts
+++ build/moduleDeploy.ts
@@ -14,6 +14,7 @@ const SSHClient = require('node-ssh');
  *
  * This depends on these `.env` variables to be set:
  * `MAGENTO_IP_ADDRESS`: IP Address for SSH connection to Magento instance
+ * `MAGENTO_SSH_PORT`: Port for SSH
  * `MAGENTO_SSH_USERNAME`: Username enabled for SSH
  * `MAGENTO_SSH_PASSWORD`: Password for SSH (either this or a private key is required)
  * `MAGENTO_SSH_PRIVATE_KEY`: Path to a private key to use instead of a password
@@ -38,6 +39,7 @@ export const moduleDeploy = (): void => {
 
   const connection: any = {
     host: process.env.MAGENTO_IP_ADDRESS,
+    port: process.env.MAGENTO_SSH_PORT,
     username: process.env.MAGENTO_SSH_USERNAME,
     path: '/var/www/html/app/code/NS8/Protect/',
   };
@@ -52,7 +54,7 @@ export const moduleDeploy = (): void => {
     .connect({
       host: connection.host,
       username: connection.username,
-      port: 22,
+      port: connection.port,
       password: connection.password,
       tryKeyboard: true,
       onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
