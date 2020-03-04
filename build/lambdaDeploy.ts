d7805dbd062a1620971cd6a14653a196da9a4bec
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-03-04 15:56:25 -0500

    Updated deploy task to use NODE_ENV for CI.

--- build/lambdaDeploy.ts
+++ build/lambdaDeploy.ts
@@ -2,7 +2,14 @@
 import { execSync } from 'child_process';
 import { env } from './loadEnv';
 
-const stage = process.env.DEV_SUFFIX ? process.env.DEV_SUFFIX : 'dev';
+let stage = 'test';
+if (process.env.NODE_ENV?.toLowerCase().startsWith('prod')) {
+  stage = 'prod';
+} else if (process.env.NODE_ENV?.toLowerCase().startsWith('dev')) {
+  stage = 'test';
+} else if (process.env.DEV_SUFFIX) {
+  stage = process.env.DEV_SUFFIX;
+}
 const method = (process.env.METHOD || 'deploy').trim().toLowerCase();
 if (method !== 'deploy' && method !== 'remove') throw new Error(`Method ${method} is not supported`);
 
