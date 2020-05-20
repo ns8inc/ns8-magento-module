4f83034291fdcc6a111c36059e5c8affbcd6b9be
Chris Doriott <chris.doriott@ns8.com>
2020-05-20 15:22:14 -0700

    Updating Cron logic to be abstracted from Polling logic CH-28362

--- Cron/Test.php
+++ Cron/Test.php
@@ -4,19 +4,11 @@ namespace NS8\Protect\Cron;
 use NS8\Protect\Polling\Client as PollingClient;
 use Psr\Log\LoggerInterface;
 
-
 /**
  * Cron-job to permit polling to NS8 Protect Services
  */
 class Test
 {
-    /**
-     * Source of data to write to logs
-     */
-    const SOURCE_DIRECTORY = '/tmp/polling/';
-    const MIN_FILE_INDEX = 0;
-    const MAX_FILE_INDEX = 100;
-
     /**
      * Logging attribute
      * @var LoggerInterface $logger
@@ -29,7 +21,8 @@ class Test
      * @param Psr\Log\LoggerInterfac $logger
      *
      */
-    public function __construct(LoggerInterface $logger) {
+    public function __construct(LoggerInterface $logger)
+    {
         $this->logger = $logger;
     }
 
@@ -42,27 +35,11 @@ class Test
     {
         try {
             PollingClient::startService();
-            $fileList = glob(self::SOURCE_DIRECTORY.'*', GLOB_NOSORT);
-            if (count($fileList) < 2) {
-                return;
 
+            $fileList = PollingClient::getFileList();
+            foreach ($fileList as $baseFile) {
+                $this->handleFile($baseFile);
             }
-
-            $file = self::SOURCE_DIRECTORY.self::MAX_FILE_INDEX;
-            $renewingFile = self::SOURCE_DIRECTORY.self::MIN_FILE_INDEX;
-            if (file_exists($file) && file_exists($renewingFile)) {
-                $this->handleFile($file);
-                return;
-            }
-
-            $fileList = array_map('basename', $fileList);
-            $fileList = array_map('intval', $fileList);
-            sort($fileList);
-            for ($i=0; $i <count($fileList)-1; $i++) {
-                $file = self::SOURCE_DIRECTORY.$fileList[$i];
-                $this->handleFile($file);
-            }
-
         } catch (\Exception $e) {
             throw $e;
         }
@@ -72,16 +49,22 @@ class Test
      * Handle the intended actions for an input file then delete it
      *
      * @param string $file The file path we want to process
+     *
      * @return bool True if successful
      */
-    protected function handleFile(string $file) : bool
+    protected function handleFile(string $fileName) : bool
     {
-        $logData = file($file);
-        foreach ($logData as $line) {
-            $this->logger->debug($line);
-        }
-        unlink($file);
+        try {
+            $filePath = PollingClient::getFullFilePath($fileName);
+            $logData = file($filePath);
+            foreach ($logData as $line) {
+                $this->logger->debug($line);
+            }
 
-        return true;
+            PollingClient::removeFile($fileName);
+            return true;
+        } catch (\Throwable $t) {
+            return false;
+        }
     }
 }
