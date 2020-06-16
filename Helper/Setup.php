76a75be399c692ca98b719e9652ed13fc5794910
Dave Hensley <dave.hensley@ns8.com>
2020-06-16 16:50:01 -0400

    Add user-friendly error message if installing on a previously-used URL without an access token. CH-32693

--- Helper/Setup.php
+++ Helper/Setup.php
@@ -144,6 +144,11 @@ class Setup extends AbstractHelper
                 ];
 
                 $installResult = InstallerClient::install('magento', $installRequestData);
+
+                if (!isset($installResult['accessToken'])) {
+                    throw new InstallException('This URL has already been registered and cannot be reused. If this is an error, please contact support@ns8.com.');
+                }
+
                 $this->config->setEncryptedConfigValue(Config::ACCESS_TOKEN_CONFIG_KEY, $installResult['accessToken']);
                 // Set a registry value so we do not attempt to fetch the token a second time
                 // if config value has not been saved yet
