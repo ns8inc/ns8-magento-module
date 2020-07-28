e4fb4cffeff752a380c720c3c9a534d22ca7bbc1
Chris Doriott <chris.doriott@ns8.com>
2020-07-28 11:25:20 -0700

    Adding initial changes for pulling queue data for multiple shops CH-36937

---
+++ Helper/Queue.php
@@ -0,0 +1,83 @@
+<?php
+namespace NS8\Protect\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use NS8\ProtectSDK\Http\Client as HttpClient;
+use NS8\ProtectSDK\Queue\Client as QueueClient;
+use Zend\Http\Client as ZendClient;
+/**
+ * Helper to assist with queue functionality
+ */
+class Queue extends AbstractHelper
+{
+    /**
+     * Fetches messages from a queue
+     *
+     * @return mixed[] Array of messages
+     */
+    public function getMessages(): array
+    {
+        try {
+            $messages = QueueClient::getMessages();
+        } catch (\Exception $e) {
+            $this->loggingClient->error('Unable to fetch messages');
+            $messages = [];
+        }
+
+        return $messages;
+    }
+
+    /**
+     * Deletes a message from a queue
+     *
+     * @param string $messageId - The ID of the message we want to delete
+     *
+     * @return bool true if the message was successfully deleted, otherwise false
+     *
+     */
+    public function deleteMessage(string $messageId) : bool
+    {
+        $returnvalue = true;
+        try {
+            QueueClient::deleteMessage($messageId);
+        } catch (\Exception $e) {
+            $this->loggingClient->error(sprintf('Unable to delete message: %s', $messageId));
+            $returnValue = false;
+        }
+
+        return $returnValue;
+    }
+
+    public function setNs8HttpClient(HttpClient $ns8HttpClient) : void
+    {
+        try {
+            QueueClient::setNs8HttpClient($ns8HttpClient);
+        } catch (\Exception $e) {
+            $this->loggingClient->error('Unable to set NS8 HTTP Client message: %s');
+            throw $e;
+        }
+    }
+
+    public function setQueueUrl(string $queueUrl) : void
+    {
+        try {
+            QueueClient::initialize(null, $orderFetchCountqueueUrl);
+        } catch (\Exception $e) {
+            $this->loggingClient->error('Unable to set Queue URL');
+            throw $e;
+        }
+    }
+
+    public function fetchQueueUrl(int $storeId) : string
+    {
+        try {
+            $this->config->initSdkConfiguration($storeId);
+            $sdkHttpClient = new HttpClient();
+            $urlData = $sdkHttpClient->post(self::GET_QUEUE_URL);
+            return $urlData->url;
+        } catch (\Exceotion $e) {
+            $this->loggingClient->error('Unable to fetch Queue URL');
+            throw $e;
+        }
+    }
+}
