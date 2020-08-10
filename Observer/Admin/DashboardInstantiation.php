6843653927999146630e506f6280e5d756205fb2
Chris Doriott <chris.doriott@ns8.com>
2020-08-09 18:23:26 -0700

    Removing testing components

--- Observer/Admin/DashboardInstantiation.php
+++ Observer/Admin/DashboardInstantiation.php
@@ -158,7 +158,7 @@ class DashboardInstantiation implements ObserverInterface
             $moduleData = $this->moduleList->getOne('NS8_Protect');
             $moduleVersion = $moduleData['setup_version'] ?? '';
             $storeEmail = $this->scopeConfig->getValue('trans_email/ident_sales/email') ?? '';
-            $storeUrl = sprintf('https://chrisdoriotttest%d.com', rand(1, 10000));  //rtrim($store->getBaseUrl(UrlInterface::URL_TYPE_WEB, true), '/');
+            $storeUrl = rtrim($store->getBaseUrl(UrlInterface::URL_TYPE_WEB, true), '/');
             $merchantId = $this->config->getMerchantId();
             $merchantId = empty($merchantId) ? $this->config->generateMerchantId() : $merchantId;
             $installRequestData = [
