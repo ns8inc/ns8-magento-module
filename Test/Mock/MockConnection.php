efd1e0f354f922d1febddd607f7e9555e23f3b17
Alex Hicks <alex@alexhicks.net>
2020-08-21 17:31:43 -0400

    Fix N/A score bug with enormous helpings of help from Cruz and Chris

---
+++ Test/Mock/MockConnection.php
@@ -0,0 +1,17 @@
+<?php
+namespace NS8\Protect\Test\Mock;
+
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+class MockConnection
+{
+  public function quoteTableAs($tableName): ?string
+  {
+    return $tableName;
+  }
+
+  public function query($query, $params): void
+  {
+    // no-op
+  }
+}
